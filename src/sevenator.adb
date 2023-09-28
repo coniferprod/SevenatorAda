6358a7c4cdb1866c6bf0fe4fe7d17e55489629b0
Jere Käpyaho <2261936+jerekapyaho@users.noreply.github.com>
2023-09-28 16:57:03 +0300

    Cleanup

--- src/sevenator.adb
+++ src/sevenator.adb
@@ -1,23 +1,11 @@
 with Ada.Text_IO; use Ada.Text_IO;
 with Ada.Command_Line;
-with Helpers; use Helpers;
-with DX7; use DX7;
-with Commands; use Commands;
+with Commands;
 
 procedure Sevenator is
     package CLI renames Ada.Command_Line;
 
 begin
-    -- Test random name generation
-    --for I in 1 .. 10 loop
-    --    Ada.Text_IO.Put_Line (Random_Voice_Name);
-    --end loop;
-
-    -- Example of echoing the command line arguments:
-    --for i in 1 .. CLI.Argument_Count loop
-    --    IO.Put_Line (Item => CLI.Argument (Number => i));
-    --end loop;
-
     if CLI.Argument_Count < 2 then
         Put_Line ("Usage: sevenator command filename");
         Put_Line ("  dump = show contents of DX7 file <filename>");
@@ -28,13 +16,12 @@ begin
 
     declare
         Command : constant String := CLI.Argument (1);
-        Name : constant String := CLI.Argument (2);
+        File_Name : constant String := CLI.Argument (2);
     begin
-        case Command_Type'Value (Command) is
-            when Dump => Run_Dump (Name);
-            when Cartridge => Commands.Run_Cartridge (Name);
-            when Voice => Commands.Run_Voice (Name);
-            --when others => Put_Line ("Unknown command: " & Command);
+        case Commands.Command_Type'Value (Command) is
+            when Commands.Dump => Commands.Run_Dump (File_Name);
+            when Commands.Cartridge => Commands.Run_Cartridge (File_Name);
+            when Commands.Voice => Commands.Run_Voice (File_Name);
         end case;
     end;
 end Sevenator;
